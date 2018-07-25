0a76f2cbd8087a80c26fadf70113f9ce4116d956
HN <hhnguyen92@gmail.com>
2018-07-24 18:32:30 -0700

    Implement if queue is already correct checker

--- array_new_year_chaos.js
+++ array_new_year_chaos.js
@@ -4,7 +4,20 @@ function minimumBribes(q) {
     correctQue.push(i);
   }
 
+  let count = 0;
+  correctQue.forEach((el, idx) => {
+    if ( correctQue[idx] === q[idx] ) {
+      count += 1;
+    }
+  });
+
+  if (count === q.length) {
+    return 0;
+  }
+
 }
+
+
 
 console.log(minimumBribes([2, 1, 5, 3, 4]));
 console.log(minimumBribes([2, 5, 1, 3, 4]));
