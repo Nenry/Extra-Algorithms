ecb10d8bec6a4be82aca3ec6c16b8f679a477475
HN <hhnguyen92@gmail.com>
2018-08-06 13:52:03 -0700

    Modify solution

--- sherlock_anagrams.js
+++ sherlock_anagrams.js
@@ -1,24 +1,40 @@
 function sherlockAndAnagrams(s) {
   let anagrams = {};
   let splitted = s.split("");
+  let pairs = 0;
   splitted.forEach((letter, idx) => {
     for (let j = idx; j < splitted.length; j++) {
       let sorted = splitted.slice(idx, j + 1).sort().join("");
+      console.log(sorted);
       if (anagrams[sorted] === undefined) {
+        console.log('this');
         anagrams[sorted] = 1;
       } else if (anagrams[sorted]) {
-        anagrams[sorted] += 1
+        console.log('that');
+        anagrams[sorted] += 1;
+        pairs += 1;
+        console.log(pairs);
       }
     }
+    // anagrams = {};
   });
 
-  let counts = Object.values(anagrams);
-  let totalCount = 0;
-  console.log(anagrams);
-  counts.forEach((el) => {totalCount += Math.floor(el / 2);});
-  return totalCount;
+  // let counts = Object.values(anagrams);
+  // let totalCount = 0;
+  // console.log(anagrams);
+
+  // counts.forEach((el) => {
+  //   console.log(el);
+  //   totalCount += el;});
+  // return Math.floor(totalCount/2);
+  return pairs;
 }
 
 
+// console.log(sherlockAndAnagrams('cdcd')); // should equal 5
 console.log(sherlockAndAnagrams('kkkk'));
 
+
+//iterate through which each length, reset after last has been reached
+
+// https: //www.hackerrank.com/rest/contests/master/challenges/sherlock-and-anagrams/hackers/ChesterDrawers/download_solution
