cb6f23f2094d2e48038134234ca52ac3cd99952b
HN <hhnguyen92@gmail.com>
2018-08-12 18:05:11 -0700

    Comment psuedo code and clarifications

---
+++ anagrams.js
@@ -0,0 +1,15 @@
+// Anagrams
+// Find mininmal amount of deletions until they are complete anagrams of each other
+// they are just purely lowercase lettrs
+//punctuations or anything special?
+//edge cases?
+//duplicates?
+
+//psuedo solutions 
+//1. Hash/set one string of all its letter// improvement hash both. 2N // while hashing can keep count of letter length
+//2. Check each letter of the longest string 
+//3. forEach(letter) (deleteCount += Math.abs(hash1[letter] - hash2[letter])
+//4. return count 
+
+
+
