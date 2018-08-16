66dd06a15cc03c0bf7a61c2a1b804f2888bcd28e
HN <hhnguyen92@gmail.com>
2018-08-15 18:57:41 -0700

    Implement psuedo code for swap_node

---
+++ swap_nodes.js
@@ -0,0 +1,34 @@
+function swapNodes(indexes, queries) {
+  let results = [];
+
+  //recurs indexes[0]
+
+    /*
+     * Write your code here.
+     */
+  
+}
+
+
+class Nodes {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+//root is always 1
+// in-order traversal
+// keep going left node
+//then go right
+
+//1. Traverse the left subtree.
+//2.  Visit root.
+//3. Traverse the right subtree
+     L + R   l + r of L  l + r of R
+// [[2, 3], [-1, -1], [-1, -1]]
+// [1, 1]
+//start with left which is 
+
+       // 1 
+      //2   3
