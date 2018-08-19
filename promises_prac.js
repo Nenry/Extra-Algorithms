370ec52b3d3cfee11e2f1488c7a3d96b7131b870
HN <hhnguyen92@gmail.com>
2018-08-19 12:16:18 -0700

    Update promises practice with comments

--- promises_prac.js
+++ promises_prac.js
@@ -1,13 +1,44 @@
- let array = [];
- const test = new Promise(function (resolve, reject) {
-   array.push('123');
+var array= [];
+
+function test() {
+  return(
+
+    new Promise(function (resolve, reject) {
+      array.push('hi');
+      //  console.log(array);
+      resolve('hello');
+    })
+  );
+}
+ const test1 = new Promise(function (resolve, reject) {
+   array.push('hello');
+  //  console.log(array);
    resolve('hello');
  });
-
-
- const promises = [test, test, test]
-
- Promise.all(promises).then(() => {
+ const promises = [test1, test1, test1];
+ Promise.all(promises).then((values) => {
+   //array will only return ['hello']
+   // if we want to make it return ['hello', 'hello', 'hello'] then a CB is needed such as function test()
    console.log(array);
-   array.forEach(el => console.log(el));
+   //values is an array of all of the resolves
+  array = values;
+  console.log(array);
  });
+//  const test2 = new Promise(function (resolve, reject) {
+//    array.push('hoe');
+//   //  console.log(array);
+//    resolve('hello');
+//  });
+
+// test().then(() => test()).then(() => test()).then(() => console.log(array));
+
+
+//  var promise1 = Promise.resolve(3);
+//  var promise2 = 42;
+//  var promise3 = new Promise(function (resolve, reject) {
+//    setTimeout(resolve, 100, 'foo');
+//  });
+
+//  Promise.all([promise1, promise2, promise3]).then(function (values) {
+//    console.log(values);
+//  });
