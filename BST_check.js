dae54c278877ef000311b23f4f1057747e855e4a
HN <hhnguyen92@gmail.com>
2018-08-29 13:45:49 -0700

    Complete BST validation problem

--- BST_check.js
+++ BST_check.js
@@ -1,35 +1,11 @@
-// var isValidBST = function (root) {
-//   //bst is valid if root.val >= root.left.val || root.left === undefined
-//   //bst is valid if root.val < root.right.val || root.right === undefined
+var isValidBST = function (root) {
+  
+  return isValidBSTHelper(root, -Infinity, Infinity);
+ 
+};
 
-//   //possible base case
-//   //if root.left 
-
-
-
-//   //return false if the conditions arent met
-//   //keep doing recursion or while loop while root is still there until root.left is null or root.right is null
-
-
-//   if (root.left.val > root.val) {
-//     return false;
-//   } else if (root.left === null) {
-//     return true;
-//   }
-
-//   if (root.right.val <= root.val) {
-//     return false;
-//   } else if (root.right === null) {
-//     return true;
-//   }
-
-//   const left = isValidBST(root.left);
-//   const right = isValidBST(root.right);
-
-//   if (!left || !right) {
-//     return false;
-//   } else {
-//     return true;
-//   }
-
-// }
+function isValidBSTHelper(root, min, max) {
+  if (!root) return true;
+  if (root.val <= min || root.val >= max) return false;
+  return isValidBSTHelper(root.left, min, root.val) && isValidBSTHelper(root.right, root.val, max);
+}
