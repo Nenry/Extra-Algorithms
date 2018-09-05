db1d2988062557894fbca9022d2dfffda3328e6f
HN <hhnguyen92@gmail.com>
2018-09-05 16:45:39 -0700

    Complete morse code algorithm

--- unique_morse_code.js
+++ unique_morse_code.js
@@ -7,14 +7,19 @@ var uniqueMorseRepresentations = function (words) {
   const alphas = { a: 0, b: 1, c: 2, d: 3, e: 4, f: 5, g: 6, h: 7, i: 8,
    j: 9, k: 10, l: 11, m: 12, n: 13, o: 14, p: 15, q: 16, r: 17, s: 18,
   t: 19, u: 20, v: 21, w: 22, x: 23, y: 24, z: 25};
-  
+  let results = {};
   const morseAlphas = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."];
 
-  let result = "";
-  for (let i = 0; i < words.length; i++) {
-    const morseIdx = alphas[words[i]];
-    result += morseAlphas[morseIdx];
-  }
+  words.forEach(word => {
+    let morseWord = "";
 
-  return result;
+
+    for (let i = 0; i < word.length; i++) {
+      const morseIdx = alphas[word[i]];
+      morseWord += morseAlphas[morseIdx];
+    }
+    results[morseWord] = true;
+  });
+
+  return results.length();
 };
