0d7697dee8de57ae7a885b7da63b6bfc7d23bb92
HN <hhnguyen92@gmail.com>
2018-09-05 17:23:29 -0700

    Plan out hammering distance

---
+++ hammering_distance.js
@@ -0,0 +1,6 @@
+//turn each into they both are either 8 bits or until one is equal length, best to check if x.length < y.length
+//while loop to constantly adding one. it will exit the loop once it is equal
+//doing !== or x === y.length is dangerous because x maybe greater and itll just be in a never ending loop
+var hammingDistance = function (x, y) {
+
+};
