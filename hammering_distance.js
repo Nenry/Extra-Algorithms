946c00bb084d4db0c4f2ef70b966bfef8e9a91a8
HN <hhnguyen92@gmail.com>
2018-09-11 17:34:10 -0700

    start hammering distance algo

--- hammering_distance.js
+++ hammering_distance.js
@@ -2,5 +2,11 @@
 //while loop to constantly adding one. it will exit the loop once it is equal
 //doing !== or x === y.length is dangerous because x maybe greater and itll just be in a never ending loop
 var hammingDistance = function (x, y) {
+let xBit = x.toString(2);
+let yBit = y.toString(2);
+
+while (xBit.length < 8 || xBit.length < yBit.length) {
+  xBit = "0" + xBit;
+}
 
 };
