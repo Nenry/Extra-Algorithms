b9b8a171700d65255fca1e05a15edb68c0ceb1d6
HN <hhnguyen92@gmail.com>
2018-09-11 23:41:20 -0700

    Complete robot return to origin

---
+++ robot_return_to_origin.js
@@ -0,0 +1,31 @@
+var judgeCircle = function (moves) {
+  let vertLoc = 0;
+  let horzLoc = 0;
+  const sideMoves = {
+    R: 1,
+    L: -1
+  }
+  const vertMoves = {
+    U: 1,
+    D: -1
+  }
+
+  for (let i = 0; i < moves.length; i++) {
+
+    if (sideMoves[moves[i]]) {
+      horzLoc += sideMoves[moves[i]]
+    } else if (vertMoves[moves[i]]) {
+      vertLoc += vertMoves[moves[i]]
+    }
+
+
+  }
+
+  if (!vertLoc && !horzLoc) {
+    return true
+  } else {
+    return false
+  }
+
+
+};
