4a3b3606112ca1597042912c690d32d1341f6bb4
HN <hhnguyen92@gmail.com>
2018-09-18 21:43:00 -0700

    Complete flip and invert image

--- flipping_an_image.js
+++ flipping_an_image.js
@@ -6,14 +6,36 @@ function invertIt(num) {
   }
 }
 
-var flipAndInvertImage = function (A) {
-  //iterate through each subarray, reverse it, could probably do iteration by half for reversing
-  // if === 1 then make it 0, else make it 1
-  //could probably reassign as it is reversing
+// var flipAndInvertImage = function (A) {
+//   //iterate through each subarray, reverse it, could probably do iteration by half for reversing
+//   // if === 1 then make it 0, else make it 1
+//   //could probably reassign as it is reversing
   
-  A.forEach(subA => {
-    for(let i = 0; i < subA.length/2; i++) {
-      
-    } 
-  });
-};
+//   A.forEach(subA => {
+//     for(let i = 0; i < subA.length/2; i++) {
+//       const temp = subA[i];
+//       subA[i] = subA[subA.length - 1 - i];
+//       subA[subA.length - 1 - i] = temp;
+//     }
+    
+//     subA.forEach((num, idx) => {
+//       subA[idx] = invertIt(num);
+//     });
+    
+//   });
+//   return A;
+// };
+
+
+var flipAndInvertImage = function(A) {
+  let result = [];
+  for (line of A) {
+    const flipped = line.reverse();
+    const inversed = flipped.map(bit => bit ? 0 : 1);
+    result.push(inversed);
+  }
+  return result;
+}
+
+
+console.log(flipAndInvertImage([[1, 1, 0], [1,0,1], [0,0,0]]));
