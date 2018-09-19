b3593aa803889619edfc6d7aac197b6784f06e1a
HN <hhnguyen92@gmail.com>
2018-09-18 21:29:47 -0700

    Psuedo code

---
+++ flipping_an_image.js
@@ -0,0 +1,19 @@
+function invertIt(num) {
+  if (num === 1) {
+    return 0;
+  } else {
+    return 1;
+  }
+}
+
+var flipAndInvertImage = function (A) {
+  //iterate through each subarray, reverse it, could probably do iteration by half for reversing
+  // if === 1 then make it 0, else make it 1
+  //could probably reassign as it is reversing
+  
+  A.forEach(subA => {
+    for(let i = 0; i < subA.length/2; i++) {
+      
+    } 
+  });
+};
