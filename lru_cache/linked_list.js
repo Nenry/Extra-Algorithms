a6ce795fb3075d2e2615c110782864299b196b9d
HN <hhnguyen92@gmail.com>
2018-09-28 10:46:23 -0700

    Complete linked list

--- lru_cache/linked_list.js
+++ lru_cache/linked_list.js
@@ -8,7 +8,71 @@ class LinkedList {
     this.head.next = this.tail;
     this.tail.prev = this.head;
   }
+
+
+//sentinel nodes, head and tail are always null nodes
+  first() {
+    return this.head.next;
+  }
+
+  last() {
+    return this.tail.prev;
+  }
+
+  isEmpty() {
+    return this.head.next === this.tail && this.tail.prev === this.head;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+    this.tail.prev.next = newNode;
+    newNode.prev = this.tail.prev;
+
+    this.tail.prev = newNode;
+    newNode.next = this.tail;
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+
+    this.head.next.prev = newNode;
+    newNode.next = this.head.next;
+
+    this.head.next = newNode;
+    newNode.prev = this.head;
+
+    return newNode;
+
+  }
+
+  find(value, node = this.head.next) {
+    if (node === null) {
+      return false;
+    }
+
+    if (node.value === value) {
+      return node;
+    } else {
+      return this.find(value, node.next);
+    }
+
+  }
+
+  remove(value, node = this.head.next) {
+    if (node === null) {
+      return null;
+    }
+
+    if(node.value === value) {
+      node.remove();
+      return node;
+    } else {
+      return this.remove(value, node.next);
+    }
+  }
+
+
+
 }
 
-
-first()
+module.exports = LinkedList;
