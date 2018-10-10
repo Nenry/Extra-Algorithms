1685ea93292933d23d4fb7f4a019bb65f243f57b
HN <hhnguyen92@gmail.com>
2018-10-10 13:56:08 -0700

    Hotfix

--- self_diving_numbers.js
+++ self_diving_numbers.js
@@ -2,7 +2,7 @@
 //self diving numbers can be divided evenly by each digit in the number, the number cannot contain a 0 in it
 var selfDividingNumbers = function (left, right) {
   const results = [];
-s
+
   for(let n = left; n <= right; n++) {
     const num = n.toString().split("").map((el) => {
       return parseInt(el);
