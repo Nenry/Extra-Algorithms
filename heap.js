ec7a7780a018fbc8bf4c7f4bc0aab90c714b703d
HN <hhnguyen92@gmail.com>
2018-10-25 17:10:28 -0700

    Start binary min heap

---
+++ heap.js
@@ -0,0 +1,19 @@
+class BinaryMinHeap {
+  constructor() {
+    this.store = [];
+  }
+
+  count() {
+    return this.store.length;
+  }
+
+  extract() {
+    //pop off the min
+    //swap first and last element, pop off
+    //heapify down
+
+  }
+
+
+  
+}
