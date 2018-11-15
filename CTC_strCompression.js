7cc6c9bc778e22293e885f6b3669faa3c2963225
HN <hhnguyen92@gmail.com>
2018-11-14 16:22:14 -0800

    Complete solution for string compression

--- CTC_strCompression.js
+++ CTC_strCompression.js
@@ -14,5 +14,29 @@
 //if counter > 1 then add counter and reset counter = 1
 // return result str
 
+function stringComp(str) {
+  let results ='';
+  let counter = 1;
 
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      counter++;
+    } else {
+      results += str[i];
+
+      if (counter > 1) {
+        results += counter.toString();
+        counter = 1;
+      }
+    }
+
+
+  }
+
+  return results;
+}
+
+
+console.log(stringComp('aabcccccaaa'));
+console.log(stringComp('aabCcCccaaa'));
 
