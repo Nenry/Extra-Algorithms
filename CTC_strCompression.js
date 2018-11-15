316802b7e8b24be00e7b991cafbb8b942cd13e58
HN <hhnguyen92@gmail.com>
2018-11-14 16:18:50 -0800

    Implement plan for string compression algorithm

---
+++ CTC_strCompression.js
@@ -0,0 +1,18 @@
+//are the only inputs strings?
+//are there only regular characters/alphabets
+//are there upper case and lower case?
+// case-sensitive?
+
+//it is assumed only upper case and lower case letters
+
+//plan: iterate through the whole string
+//have a counter for letter occurences
+//create result str
+// second pointer or an i + 1
+// if str[i] === str[i + 1], then increment counter
+// if they do not then add str[i]
+//if counter > 1 then add counter and reset counter = 1
+// return result str
+
+
+
