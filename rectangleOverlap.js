bad1e5dc52b8e3144dd5332e639379bdc701b5e9
HN <hhnguyen92@gmail.com>
2018-11-20 22:07:55 -0800

    Transfer rectangle overlap and rotate matrix

---
+++ rectangleOverlap.js
@@ -0,0 +1,8 @@
+//you will be given 4 coordinates, top left and bottom right of each rectangle
+//Find some type of range and see if the coordinates exist within that range
+// Multiple ways a rectangle can overlap.
+//Can think about the other way, two rectangles, will either  side by side or top and bottom. If it will always be parallel to the axis
+
+function rectangleOverlap(topleft1, bottomright1, topleft2, bottomright2) {
+
+}
