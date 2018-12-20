0bad18aff4a1039c39e1d0c18913a4b4ebc2f4bb
HN <hhnguyen92@gmail.com>
2018-12-19 21:26:44 -0800

    Comment game plan for braces

--- braces.js
+++ braces.js
@@ -1,4 +1,5 @@
 // For example, [{}] is a valid grouping of braces but [}]{} is not.
+//iterate through the array, push open brace, if it closed, then check last of the array,
 function braces(values) {
 
 
