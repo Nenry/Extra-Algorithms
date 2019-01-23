048ee35d11a5949a3940b8d168118a3cd79e8d6e
HN <hhnguyen92@gmail.com>
2019-01-23 10:52:56 -0800

    Complete rooms and keys algorithm"

--- DFS.js
+++ DFS.js
@@ -10,6 +10,7 @@ class Node {
   }
 
   depthFirstSearch(array) {
+    //time complexity is O(V + E) => vertices + edges, Space complexity is O(V) since the array is being created and itll just be each vertice
     array.push(this.name);
     for (let child of this.children) {
       child.depthFirstSearch(array);
